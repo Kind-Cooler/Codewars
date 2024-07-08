0de5254aacd4d098c431151010fb3ae64a221f17
Neu Balance <neubal@proton.me>
2024-07-08 10:37:52 -0600

    .

--- C/dirreduct.c
+++ C/dirreduct.c
@@ -11,28 +11,33 @@ const char **dirReduc(const char *const array[], int length_in, int *length_out)
   Your array will be freed, but the strings will not be.
   Report the length of your array through the 'length_out' pointer.
 */
-    int i, j, x = 0;
-    int mask [length_in];
+    int x = 0;
     
     char **reduced = malloc(length_in * sizeof *reduced);
-    
+/*    
     for (i = 0; i < length_in; i++){
         printf ("%s\n", array[i]);
-        if (array[i] == "NORTH" && array[i+1] == "SOUTH"){
+        if ((strcmp(array[i],"NORTH")!= 0 && strcmp(array[i+1], "SOUTH")) != 0){
             ;
         }
         else
-        strcpy (reduced[x++], array[i+2]);
+            strcpy (reduced[x++], array[i]);
+
+        if ((strcmp(array[i],"EAST")!= 0 && strcmp(array[i+1], "WEST")) != 0){
+            ;
+        }
+        else
+            strcpy (reduced[x++], array[i]);
     } 
 
     
-    for (i = 0; i < length_in; i++){
-        printf ("%s\n", array[i]);
+    for (i = 0; i < x; i++){
+        printf ("%s\n", reduced[i]);
     }
-
-  //const char **reduced = malloc(2 * sizeof *reduced);
-  //reduced[0] = "SOUTH";
-  //reduced[1] = "NORTH";
+*/
+/*  const char **reduced = malloc(2 * sizeof *reduced);
+  reduced[0] = "SOUTH";
+  reduced[1] = "NORTH";*/
   *length_out = x;
   return reduced;
 }
